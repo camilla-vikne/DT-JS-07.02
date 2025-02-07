4134583eb5a6eedbfd3b3d9cdcd1ac9f27581295
camillab09 <camillab09.kodehode@gmail.com>
2025-02-07 10:44:22 +0100

    First committ

---
+++ script.js
@@ -0,0 +1,50 @@
+
+//Condotionals
+// let alder = 16;
+
+// if(alder >= 18) {
+//     console.log("Du er gammel nok til [ komme inn!")
+// }
+
+// if( alder >= 18) {
+//     console.log("Du slipper inn")
+// } else {
+//     console.log("Du er ikke gammel nok!")
+// }
+
+// let age = 20;
+// let name = "Hans";
+
+// if(age === 18)
+// {
+//     console.log("Du er gammel nok!")
+// } else if (age === 16){
+//     console.log("Du er ikke gammel nok")
+// } else{
+//     console.log("Try again!")
+// }
+
+// let bruker = {
+//     navn: "Ole",
+//     alder: 25,
+//     by: "Oslo"
+// } 
+
+// if(bruker.name === "John"){
+//     console.log("Welcome, John")
+// } else if (bruker.name === "Ole" && bruker.age === 30){
+//     console.log("Hei Ole, du er 30!")
+// } else if (bruker.name === "Ole" || 
+//     bruker.by === "Bergen") {
+//         console.log("Welcome, Ole!")
+//     } else {
+//         console.log("Wrong user input")
+//     }
+
+// ternary operator
+let name = " Ida ";
+let age = 20;
+
+let melding = age >= 18 ? "Du er voksen" : "Du er barn";
+let finalMessage = melding + name
+console.log(finalMessage);
